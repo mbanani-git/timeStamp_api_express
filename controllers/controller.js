483a0ea903b63d69b1123c8b6f9ae1c6f2e99669
mbanani-git <leedbn1@gmail.com>
2021-12-30 23:04:18 +0100

    ok

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,14 +7,16 @@ const sendUnix = (req, res) => {
     if (time.includes("-")) {
       if (moment(time).isValid()) {
         let utc = moment.utc(time).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
-        let unix = moment.utc(time).format("x");
+        let unix = Number(moment.utc(Number(time)).format("x"));
+
         return res.status(201).json({ unix, utc });
       }
       return res.status(401).json({ error: "Invalid Date" });
     } else {
       if (moment(Number(time)).isValid()) {
         let utc = moment.utc(Number(time)).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
-        let unix = moment.utc(Number(time)).format("x");
+        let unix = Number(moment.utc(Number(time)).format("x"));
+
         return res.status(201).json({ unix, utc });
       }
       return res.status(401).json({ error: "Invalid Date" });
