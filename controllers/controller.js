5ddecdd188cc84e6b5d6358d5886e9b7f840ddf6
mbanani-git <leedbn1@gmail.com>
2021-12-31 13:08:30 +0100

    ok

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,28 +2,29 @@ var moment = require("moment"); // require
 moment().format();
 
 const sendUnix = (req, res) => {
-  const { time } = req.params;
-  console.log(req.params);
-  // if (time === undefined) {
-  //   time = new Date();
-  //   console.log(time);
-  // }
-  if (time.includes("-")) {
-    if (moment(time).isValid()) {
-      let utc = moment.utc(time).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
-      let unix = Number(moment.utc(Number(time)).format("x"));
+  try {
+    const { time } = req.params;
+    // let t = new Date("2015-12-25");
+    // console.log(t);
+    if (time.includes("-")) {
+      if (moment(time).isValid()) {
+        let utc = moment.utc(time).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
+        let unix = Number(moment.utc(time).format("x"));
 
-      return res.status(201).json({ unix, utc });
-    }
-    return res.status(401).json({ error: "Invalid Date" });
-  } else {
-    if (moment(Number(time)).isValid()) {
-      let utc = moment.utc(Number(time)).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
-      let unix = Number(moment.utc(Number(time)).format("x"));
+        return res.status(201).json({ unix, utc });
+      }
+      return res.status(401).json({ error: "Invalid Date" });
+    } else {
+      if (moment(Number(time)).isValid()) {
+        let utc = moment.utc(Number(time)).format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
+        let unix = moment.utc(Number(time)).format("x");
 
-      return res.status(201).json({ unix, utc });
+        return res.status(201).json({ unix, utc });
+      }
+      return res.status(401).json({ error: "Invalid Date" });
     }
-    return res.status(401).json({ error: "Invalid Date" });
+  } catch (error) {
+    console.log(error);
   }
 };
 const emptyDate = (req, res) => {
